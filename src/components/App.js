8ba691fa856fdc74fb5f061ff780fcc2d24c4581
Aidan Byrne <aidanbyrne142@gmail.com>
2020-12-29 18:36:12 +0000

    Added Event Handlers to Search Bar
    - Added event handlers to search bar to allow for callbacks between components

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 
-const App = () =>{
-    return(
-        <div className="ui container mt" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    )
+class App extends React.Component{
+    
+    onSearchSubmit(searchInput){
+        console.log(searchInput);
+    }
+
+    render(){
+        return(
+            <div className="ui container mt" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component{
+    
+    state = {inputValue: ''};
+
+    onInputChange = (e) => {
+        this.setState({inputValue: e.target.value});
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        
+        this.props.onSearchSubmit(this.state.inputValue);
+    }
+
     render(){
         return(
             <div className="ui segment">
-                <from className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            onChange={this.onInputChange} 
+                            value={this.state.inputValue} 
+                        />
                     </div>
-                </from>
+                </form>
             </div>
         )
     }
